8b8b0457e8201ed1741a0e4a7341384f489657e1
2k2p <126388431+2k2p@users.noreply.github.com>
2023-02-26 16:19:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+
+streamlit.header('Beeakfast Menu')
+
+Streamlit.text('omega 3 blueberry')
+Streamlit.text('2 pani puri')
+Streamlit.text('31 latte')
