b9e9f67007d415748aca44d541122087f0901cc8
2k2p <126388431+2k2p@users.noreply.github.com>
2023-02-26 16:21:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My parents new healthy diner')
 
-streamlit.header('Beeakfast Menu')
-streamlit.text('omega 3 blueberry')
-streamlit.text('2 pani puri')
-streamlit.text('31 latte')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
