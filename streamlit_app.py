5d86e82fcee4eeddaf2ee0acfc163771ec42ea58
2k2p <126388431+2k2p@users.noreply.github.com>
2023-02-27 12:22:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title('My parents new healthy diner')
 
@@ -19,3 +20,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 
 streamlit.dataframe(fruits_to_show)
+
+
+fruityvice_responce = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_responce)
